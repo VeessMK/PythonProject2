6965caec883808756388c5a46546ebf66cc27001
Matvey Kuznetsov <veess.mk@gmail.com>
2025-08-18 22:11:16 +0800

    Homework 10.1 ver_1

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,6 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def get_date(full_date: str) -> str:
     """
     Принимает формат "2024-03-11T02:26:18.671407"
@@ -5,4 +8,28 @@ def get_date(full_date: str) -> str:
     "ДД.ММ.ГГГГ"
     """
 
-    return f'{full_date[8:10]}.{full_date[5:7]}.{full_date[:4]}'
+    return f"{full_date[8:10]}.{full_date[5:7]}.{full_date[:4]}"
+
+
+def mask_account_card(account_card: str) -> str:
+    """
+    Принимать — строку, содержащую тип и номер карты или счета.
+    Возвращать строку с замаскированным номером.
+    В формате:
+    Для карт: XXXX XX** **** XXXX
+    Для счетов: **XXXX
+    """
+
+    separator = account_card.split(" ")
+    if len(separator) < 2:
+        return "Некорректный ввод"
+
+    account_card_type = " ".join(separator[:-1])
+    account_card_number = separator[-1]
+
+    if "Счет" in account_card_type:
+        masked_number = get_mask_account(account_card_number)
+    else:
+        masked_number = get_mask_card_number(account_card_number)
+
+    return f"{account_card_type} {masked_number}"
