232dd675168f25dd59d15d89ca6ced726fef7b9d
Matvey Kuznetsov <veess.mk@gmail.com>
2025-08-18 21:12:33 +0800

    function get_date, checks main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date
+
+print(get_mask_card_number("7000792289606361"))
+
+print(get_mask_account("73654108430135874305"))
+
+print(get_date("2024-03-11T02:26:18.671407"))
