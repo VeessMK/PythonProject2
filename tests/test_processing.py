b545aa80658b397cdd17cd892a2aaaaa04099349
Matvey Kuznetsov <veess.mk@gmail.com>
2025-09-01 21:23:56 +0800

    Homework10_2

---
+++ tests/test_processing.py
@@ -0,0 +1,74 @@
+from typing import Any, Dict, List
+
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filtering_by_state(list_for_filter_by_state: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    assert filter_by_state(list_for_filter_by_state, "EXECUTED") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filtering_by_canceled_state(list_for_filter_by_state: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    assert filter_by_state(list_for_filter_by_state, "CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.mark.parametrize(
+    "dict_list, state, expected",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+    ],
+)
+def test_different_state_parameter(
+    dict_list: List[Dict[str, Any]], state: str, expected: List[Dict[str, Any]]
+) -> List[Dict[str, Any]]:
+    assert filter_by_state(dict_list, state) == expected
+
+
+def test_sorting_by_date(list_for_sorting_by_date: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    assert sort_by_date(list_for_sorting_by_date) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_reverse_sorting_by_date(list_for_sorting_by_date: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    assert sort_by_date(list_for_sorting_by_date, reverse=False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
